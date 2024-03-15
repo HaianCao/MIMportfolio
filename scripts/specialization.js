dd9441411bfe9960532a825b7b85860ac76dc382
HaianCao <caohaian2005@gmail.com>
2024-03-15 23:53:48 +0700

    optimize specialization for mobile

--- scripts/specialization.js
+++ scripts/specialization.js
@@ -5,3 +5,20 @@ function changeSpecialization(specialization){
     }
     document.getElementsByClassName(specialization)[0].style.display = 'block';
 }
+
+function changeSpecializationMobile(specialization){
+    let now = document.getElementsByClassName(specialization)[0];
+    let all = document.getElementsByClassName('specialization-each');
+    for(let i = 0; i < all.length; i++){
+        if (all[i] == now){
+            continue;
+        }
+        all[i].style.display = 'none';
+    }
+
+    if(window.getComputedStyle(now).display == "none"){
+        now.style.display = "block";
+    } else {
+        now.style.display = "none";
+    }
+}
